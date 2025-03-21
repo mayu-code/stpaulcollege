532144d7d6428a9e40332deb17e972ad0a57e1e3
mayu-code <durbulemayu@gmail.com>
2025-03-21 18:16:12 +0530

    receipt

--- src/main/java/com/main/stpaul/controller/AccountantController.java
+++ src/main/java/com/main/stpaul/controller/AccountantController.java
@@ -1,5 +1,34 @@
 package com.main.stpaul.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.main.stpaul.dto.ResponseDTO.DataResponse;
+import com.main.stpaul.helper.PdfGenerator;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/accountant")
 public class AccountantController {
     
+    @GetMapping("/student/payment/receipt/{receiptId}")
+    public ResponseEntity<?> downloadPdf(@PathVariable("receiptId")String receiptId)throws Exception{
+        try {
+            DataResponse response = DataResponse.builder()
+                                                .data(PdfGenerator.generateReceiptPdf())
+                                                .message("payment Receipt get Successfully !")
+                                                .status(HttpStatus.OK)
+                                                .statusCode(200)
+                                                .build();
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
 }
