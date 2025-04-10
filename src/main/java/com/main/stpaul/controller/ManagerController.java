74c3cf68d81d9b0a373511bb2549d798655ee81a
mayu_code <durbulemayu@gmail.com>
2025-04-10 15:11:34 +0530

    sync

--- src/main/java/com/main/stpaul/controller/ManagerController.java
+++ src/main/java/com/main/stpaul/controller/ManagerController.java
@@ -748,7 +748,7 @@ public class ManagerController {
         }
     }
 
-    @DeleteMapping("/students/delete")
+    @PostMapping("/students/delete")
     @Operation(summary = "Delete students by IDs", description = "Deletes multiple students by their IDs")
     public ResponseEntity<?> deleteStudents(@RequestBody List<String> studentIds) throws Exception {
         log.info("Starting deleteStudents method with studentIds: {}", studentIds);
